ae9f5cf237927cf7219791372cb1989133152e08
unknown <atul_0012@yahoo.co.in>
2022-01-13 16:25:19 +0530

    cheers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React , {useState, useEffect} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListheading';
 
-function App() {
+const App =() =>{
+  const [movies,setMovies] = useState([
+    {
+      Title:'Stars Wars',
+      year:'1990',
+      imdbID:'tt3896198',
+
+      Rating:"",
+      Reviews:"",
+      Poster:'https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
+    },
+    {
+      Title:'Stars Wars',
+      year:'1990',
+      imdbID:'tt3896198',
+      Rating:"",
+      Reviews:"",
+      Poster:'https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
+    }
+  ]);
+  const getMovieRequest = async () => {
+    const url = ` http://www.omdbapi.com/?i=tt3896198&apikey=1d44a2a9`;
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+    if (responseJson.Search){
+       setMovies(responseJson.Search)
+    }
+  }
+ useEffect(()=>{
+   getMovieRequest();
+ } , []);   
+   
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="container movie-app">
+      <div className='row d-flex align-items-center mb-4 mt-4'>
+        <MovieListHeading heading = 'Movies'/>
+        <MovieList movies = {movies}/>
 
+      </div>
+    </div>
+  )
+}
 export default App;
